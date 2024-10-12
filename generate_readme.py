0c4dde60a1c09e142cfb6de9e22551791245643f
Venti <39846601+Vbaethon@users.noreply.github.com>
2024-10-13 00:02:54 +0800

    Update generate_readme.py

--- generate_readme.py
+++ generate_readme.py
@@ -26,7 +26,7 @@ for icon_file in os.listdir(icon_dir):
         html_content += f'''
         <td align="center" style="padding: 10px;">
             <img src="{icon_path}" alt="{icon_file}" width="60" height="60"><br>
-            <span style="font-size: 8px;"><b>{icon_file}</b>
+            <small><b>{icon_file}</b>
         </td>'''
         col_count += 1
 
